760bd4a9f18d964c50cc539026cc1fe7c64f48a3
DulanjayaSandaruwan <dulanjayasandaruwan1998@gmail.com>
2022-10-06 11:29:47 +0530

    add a table

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,23 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Employee {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+}
+
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
-  styleUrls: ['./about-us.component.scss']
+  styleUrls: ['./about-us.component.scss'],
 })
 export class AboutUsComponent implements OnInit {
+  //create an array
+  employees: Employee[] = [
+    { id: '1', name: 'Dulan', email: 'dulan@gmail.com', password: '1234' },
+    { id: '2', name: 'Sanda', email: 'sanda@gmail.com', password: '5678' },
+    { id: '3', name: 'Deegalla', email: 'deegalla@gmail.com', password: '9012' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  show(){
-    alert('Hello Dulan , welcome to Angular')
-    console.log('DulanS')
-  }
-
-  show1(){
-    alert('Work hard and win')
-  }
 }
